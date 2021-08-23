01d6fb2056a1727a1a08bd66ddb9a45169cf2983
Dewey <hammersdrichard@gmail.com>
2021-08-23 11:50:18 -0700

    commit tree 742d81e962dfc217db4cee7b9bb0df9e26e2072c

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'nvqw1w1ARprCCEC9gXAMw2AAaL8aMjwI';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = { 
+    reviews: [],
+    searchTerm: ''
+  }
+
+  render() {
+    return (
+      <div className='searchable-movie-reviews'>
+        <form>
+          <input type='text' />
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res => res.json())
+    .then(json => this.setState({ reviews: json.results }))
+  }
+}
